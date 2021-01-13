75c0fdb801a9f7bc47e77ea0fe6611953dce7e41
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-14 08:13:59 +0900

    update

--- src/login-instagram.ts
+++ src/login-instagram.ts
@@ -1,1 +1,46 @@
+import puppeteer from "puppeteer";
+import fs from "fs";
+
+const instagram_url = "https://www.instagram.com/";
+
 export {};
+
+const main = async (browser: puppeteer.Browser) => {
+  const page = await browser.newPage();
+  const wait = async (target: string) => {
+    await page.waitFor(target);
+  };
+  const input = async (target_selector: string, content: string) => {
+    await page.type(target_selector, content);
+  };
+
+  const loginSelector =
+    "#loginForm > div > div:nth-child(1) > div > label > input";
+
+  const passSelector =
+    "#loginForm > div > div:nth-child(2) > div > label > input";
+  const buttonSelector = "#loginForm > div > div:nth-child(3) > button";
+
+  await wait(loginSelector);
+  await wait(passSelector);
+  await wait(buttonSelector);
+  await input(loginSelector, process.env.USER!!);
+  await input(passSelector, process.env.PASS!!);
+  await page.click(buttonSelector);
+};
+
+(async () => {
+  const browser = await puppeteer.launch({
+    // headless: process.env.HEADLESS && false || true
+    headless: false,
+  });
+
+  // const page = await browser.newPage()
+  try {
+    await main(browser);
+  } catch (e) {
+    console.log(`err: ${e}`);
+  } finally {
+    // await browser.close();
+  }
+})();
