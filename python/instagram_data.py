47defb1a36a608184cc454f9cd4df934ffd3a916
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-22 10:28:27 +0900

    add instgram data

--- python/instagram_data.py
+++ python/instagram_data.py
@@ -1,4 +1,20 @@
 class InstagramData:
+    def __str__(self):
+        print(f"""
+==================================================
+            media: {self.media}
+==================================================
+            is_video: {self.is_video}
+==================================================
+            caption: {self.caption}
+==================================================
+            profile_url: {self.profile_url}
+==================================================
+            username: {self.username}
+==================================================
+            full_name: {self.full_name}
+==================================================
+        """)
     def __init__(self, 
     media,
     is_video,
@@ -13,3 +29,22 @@ class InstagramData:
         self.profile_url = profile_url
         self.username = username
         self.full_name = full_name
+
+def convert_to_instagram_data(oj):
+    media = oj.graphql.shortcode_media.display_url
+    caption = oj.graphql.shortcode_media.edge_media_to_caption.edges[0].node.text
+    is_video = oj.graphql.shortcode_media.is_video
+    profile_url = oj.graphql.shortcode_media.owner.profile_pic_url
+    username = oj.graphql.shortcode_media.owner.username
+    full_name = oj.graphql.shortcode_media.owner.full_name
+    return InstagramData(
+        media,
+        caption,
+        is_video,
+        profile_url,
+        username,
+        full_name
+    )
+if __name__ == "__main__":
+    from sample_insta_obj import sample_isnta_obj
+    print(convert_to_instagram_data(sample_isnta_obj))
