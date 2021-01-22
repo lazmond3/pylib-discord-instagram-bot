c8901cdeaf83a6ded1f6bc36bdfe005a0b8f14cd
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-22 10:22:34 +0900

    add instagram data

---
+++ python/instagram_data.py
@@ -0,0 +1,15 @@
+class InstagramData:
+    def __init__(self, 
+    media,
+    is_video,
+    caption,
+    profile_url,
+    username,
+    full_name
+    ):
+        self.media = media
+        self.is_video = is_video
+        self.caption = caption
+        self.profile_url = profile_url
+        self.username = username
+        self.full_name = full_name
