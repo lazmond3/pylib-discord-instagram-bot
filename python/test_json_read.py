c4dd255a1a60983860504addddc5c9c977dabbbf
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-22 10:16:57 +0900

    add test json read

--- python/test_json_read.py
+++ python/test_json_read.py
@@ -3,7 +3,7 @@ import os
 UPDATE=(os.getenv("UPDATE"))
 DEBUG=(os.getenv("DEBUG"))
 
-if __name__ == "__main__":
+def test_easy():
     js = """
     {
         "name": "test value"
@@ -12,3 +12,15 @@ if __name__ == "__main__":
     if DEBUG:
         print(json.loads(js))
     assert(json.loads(js) == dict({"name": "test value"}))
+
+if __name__ == "__main__":
+    with open("out.json") as f:
+        text = "".join([x for x in f.readlines()])
+        if DEBUG:
+            print(text[:1000])
+        
+        js = json.loads(text)
+        if DEBUG:
+            print("js object:")
+            print(js)
+            print("js object:")
