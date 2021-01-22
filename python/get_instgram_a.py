22dba52dd8986841d83bb5cedbf0202634cf0a87
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-22 09:01:26 +0900

    return make __a lik

--- python/get_instgram_a.py
+++ python/get_instgram_a.py
@@ -1,1 +1,12 @@
 # __a にアクセスする関数
+import re
+
+def make_a_url(url):
+    # https://www.instagram.com/p/CJ8u5PCH-WG/?utm_source=ig_web_copy_link
+    m = re.match(r"https://www.instagram.com/p/([^/])+", url)
+    if m:
+        new_url = m.group(0) + "/?__a=1"
+        return new_url
+    return None
+if __name__ == "__main__":
+        print("make: ", make_a_url("https://www.instagram.com/p/CJ8u5PCH-WG/?utm_source=ig_web_copy_link"))
