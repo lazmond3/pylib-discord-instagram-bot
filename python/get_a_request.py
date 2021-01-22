f44abafc4f70a1a7132ef1a52bf60d30b9e1e704
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-22 09:24:56 +0900

    redis get requets

---
+++ python/get_a_request.py
@@ -0,0 +1,45 @@
+import requests
+from redis_cli import store_data, get_data
+import os
+import json
+UPDATE=(os.getenv("UPDATE"))
+DEBUG=(os.getenv("DEBUG"))
+# グローバルでクッキーを読み込む
+cookie = dict()
+with open("cookie2.txt") as f:
+    alltext = []
+    for line in f.readlines():
+        alltext.append(line.strip())
+    text = "".join(alltext)
+    splited = text.split("; ")
+    for each in splited:
+        if DEBUG:
+            print("split: ", each.split("="))
+        key,v = each.split("=")[0], "".join(each.split("=")[1:])
+        cookie[key] = v
+
+# return dictionary
+def get_json_from_url(url):
+    key_name = "json_"+url
+    if DEBUG:
+        print("key_name: ", key_name)
+    text = ""
+    if UPDATE:
+        data = requests.get(url, cookies = cookie)
+        store_data(key_name, data.text)
+        text = data.text
+    else:
+        text = get_data(url)
+        if text == None:
+            data = requests.get(url, cookies = cookie)
+            store_data(key_name, data.text)
+            text = data.text
+
+    print(text)
+    js_dic = json.loads(text)
+    return js_dic
+
+if __name__ == "__main__":
+    js = get_json_from_url("https://www.instagram.com/p/CJ8u5PCH-WG/?__a=1")
+    print("js: ")
+    print(js)
