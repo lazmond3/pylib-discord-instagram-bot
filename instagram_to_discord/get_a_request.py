d836ae29aea5ed66ea2512b6b5f50f9189ae8f3d
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-24 12:14:30 +0900

    format

--- instagram_to_discord/get_a_request.py
+++ instagram_to_discord/get_a_request.py
@@ -2,7 +2,8 @@ import requests
 from redis_cli import store_data, get_data
 import json
 from debug import DEBUG, UPDATE
-# グローバルでクッキーを読み込む
+
+
 cookie = dict()
 with open("cookie2.txt") as f:
     alltext = []
@@ -13,29 +14,32 @@ with open("cookie2.txt") as f:
     for each in splited:
         if DEBUG:
             print("split: ", each.split("="))
-        key,v = each.split("=")[0], "".join(each.split("=")[1:])
+        key, v = each.split("=")[0], "".join(each.split("=")[1:])
         cookie[key] = v
 
 # この関数: return dictionary
+
+
 def get_json_from_url(url):
     key_name = "json_"+url
     if DEBUG:
         print("key_name: ", key_name)
     text = ""
     if UPDATE:
-        data = requests.get(url, cookies = cookie)
+        data = requests.get(url, cookies=cookie)
         store_data(key_name, data.text)
         text = data.text
     else:
         text = get_data(url)
         if text == None:
-            data = requests.get(url, cookies = cookie)
+            data = requests.get(url, cookies=cookie)
             store_data(key_name, data.text)
             text = data.text
 
     print(text)
     js_dic = json.loads(text)
     return js_dic
+
 
 if __name__ == "__main__":
     js = get_json_from_url("https://www.instagram.com/p/CJ8u5PCH-WG/?__a=1")
