448c0e97a5f6d98474446d67751988fd0d79d86c
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-24 13:38:41 +0900

    update cookie_value

--- instagram_to_discord/cookie_requests.py
+++ instagram_to_discord/cookie_requests.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from debug import DEBUG
 from .redis_cli import store_data, get_data
 
 # クッキーの使い方がわかるファイル
@@ -7,19 +8,29 @@ from .redis_cli import store_data, get_data
 # cookie2.txt をこのように使うと、クッキーを使ったリクエストができるっぽい！
 # DANGER instagram の cookie が別サイトにも送信されてしまいうる。
 cookie = dict()
-with open("cookie2.txt") as f:
-    alltext = []
-    for line in f.readlines():
-        alltext.append(line.strip())
-    text = "".join(alltext)
-    splited = text.split("; ")
-    for each in splited:
-        print("split: ", each.split("="))
-        key, v = each.split("=")[0], "".join(each.split("=")[1:])
-        cookie[key] = v
 
+
+def make_cookie(path):
+    global cookie
+    with open(path) as f:
+        alltext = []
+        for line in f.readlines():
+            alltext.append(line.strip())
+        text = "".join(alltext)
+        splited = text.split("; ")
+        for each in splited:
+            if DEBUG:
+                print("split: ", each.split("="))
+            key, v = each.split("=")[0], "".join(each.split("=")[1:])
+            cookie[key] = v
+    return cookie
+
+
+make_cookie("cookie2.txt")
 
 # redis 機能を加える
+
+
 def requests_get_cookie(url, expire=1000):
     cache = get_data(url)
     if cache:
