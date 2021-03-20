ad20f7332dc7fbf47192f90281491e3da147e584
lazmond3 <moikilo00+xcode@gmail.com>
2021-03-20 15:01:50 +0900

    ADD main controller

--- instagram_to_discord/discord_event_listener.py
+++ instagram_to_discord/discord_event_listener.py
@@ -7,6 +7,7 @@ from .string_util import sophisticate_string
 from .converter_instagram_url import instagram_make_author_page, instagram_make_base_url, instagram_extract_from_content
 from .converter_instagram_url import convert_instagram_url_to_a
 from .cookie_requests import requests_get_cookie
+from .twitter_multiple import twitter_line_to_image_urls
 
 
 class DiscordMessageListener(discord.Client):
@@ -29,6 +30,13 @@ class DiscordMessageListener(discord.Client):
                          url=instagram_make_author_page(obj.username),
                          icon_url=obj.profile_url
                          )
+        return embed
+
+    def create_embed_twitter_image(self, image_url: str):
+        embed = discord.Embed(
+            color=discord.Color.blue()
+        )
+        embed.set_image(url=image_url)
         return embed
 
     async def on_message(self, message):
@@ -54,10 +62,25 @@ class DiscordMessageListener(discord.Client):
         elif not "instagram-support" in message.author.display_name and \
             ("https://twitter.com/" in message.content and
                  "/status/" in message.content):
-            pass
-
-
-
+            content = message.content
+            msg_list = message.content.split()
+            if len(msg_list) > 1:
+                nums = msg_list[1].split(",")
+                nums = map(lambda x: int(x), nums)
+                nums = filter(lambda x: x != 1, nums)
+                nums = list(nums)
+            else: return
+            image_urls = twitter_line_to_image_urls(content)
+            for n in nums:
+                idx = n-1
+                assert(idx >= 0)
+                assert(idx < 4)
+                if len(image_urls) < n:
+                    continue
+                if n == 1: continue
+                print(f"send_twitter_image: url: {image_urls[idx]}")
+                embed = self.create_embed_twitter_image(image_urls[idx])
+                await message.channel.send(embed=embed)
 
 def main():
     client = DiscordMessageListener()
