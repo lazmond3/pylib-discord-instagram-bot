1158918b6c5fd11a5f2ac962521820883738e6da
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-12 11:46:00 +0900

    [python] delete comment

--- lambda_sample/main.py
+++ lambda_sample/main.py
@@ -1,10 +1,6 @@
 import aws_lambda_typing as lambda_typing
 
 def handler(event: lambda_typing.APIGatewayProxyEventV1, context: lambda_typing.Context) -> lambda_typing.APIGatewayProxyResponseV1:
-    # for record in event['Records']:
-    #     print(context.get_remaining_time_in_millis())
-    #     print(record['body'])
-
     print("event: ", event)
     return lambda_typing.APIGatewayProxyResponseV1({
         'isBase64Encoded': False,
