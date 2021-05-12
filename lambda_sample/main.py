0437f4d72ff2774ffc8492e14f175fcca7475a7b
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-13 00:10:20 +0900

    [python] delete lambda_sample/main.py lambda では動作しなさそうなので、破棄する。

--- lambda_sample/main.py
+++
@@ -1,6 +0,0 @@
-import aws_lambda_typing as lambda_typing
-
-def handler(event: lambda_typing.SQSEvent, context: lambda_typing.Context) -> None:
-    for record in event['Records']:
-        print(context.get_remaining_time_in_millis())
-        print(record['body'])
