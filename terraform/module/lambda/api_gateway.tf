9b7a0eda186cb575c31c291ec88c0c6a6373c4ff
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-12 14:23:14 +0900

    [terraform] fix api_gateway add deployment

--- terraform/module/lambda/api_gateway.tf
+++ terraform/module/lambda/api_gateway.tf
@@ -27,3 +27,16 @@ resource "aws_api_gateway_integration" "lambda" {
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda_function.invoke_arn
 }
+
+resource "aws_api_gateway_deployment" "example" {
+  depends_on = [
+    aws_api_gateway_integration.lambda
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.discord_endpoint.id
+  stage_name  = "test"
+}
+
+output "base_url" {
+  value = aws_api_gateway_deployment.example.invoke_url
+}
