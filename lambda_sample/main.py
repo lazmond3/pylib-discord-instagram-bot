cd786aa3211b0fe2753cd9050c27398b61d6972b
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-12 11:44:09 +0900

    [python] update lambda api gateway

--- lambda_sample/main.py
+++ lambda_sample/main.py
@@ -1,6 +1,14 @@
 import aws_lambda_typing as lambda_typing
 
-def handler(event: lambda_typing.SQSEvent, context: lambda_typing.Context) -> None:
-    for record in event['Records']:
-        print(context.get_remaining_time_in_millis())
-        print(record['body'])
+def handler(event: lambda_typing.APIGatewayProxyEventV1, context: lambda_typing.Context) -> lambda_typing.APIGatewayProxyResponseV1:
+    # for record in event['Records']:
+    #     print(context.get_remaining_time_in_millis())
+    #     print(record['body'])
+
+    print("event: ", event)
+    return lambda_typing.APIGatewayProxyResponseV1({
+        'isBase64Encoded': False,
+        'statusCode': 200,
+        'headers': {},
+        'body': '{"message": "Hello from AWS Lambda"}'
+    })
