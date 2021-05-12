cbd986ba2505d5946b019b742a25e164fdf79b38
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-12 16:55:48 +0900

    [terraform] aws_api_gateway_method で http_method = ANY にしたら、API ゲートウェイがセキュリティなしで利用できた

--- terraform/module/lambda/api_gateway.tf
+++ terraform/module/lambda/api_gateway.tf
@@ -15,9 +15,10 @@ resource "aws_api_gateway_resource" "hello_world" {
 resource "aws_api_gateway_method" "hello_world" {
   rest_api_id      = aws_api_gateway_rest_api.discord_endpoint.id
   resource_id      = aws_api_gateway_resource.hello_world.id
-  http_method      = "POST"
+  http_method      = "ANY"
+  # http_method      = "POST"
   authorization    = "NONE"
-  api_key_required = true
+  # api_key_required = true
 }
 
 resource "aws_api_gateway_method_response" "hello_world" {
--- terraform/module/lambda/api_gateway_key.tf
+++ terraform/module/lambda/api_gateway_key.tf
@@ -1,20 +1,20 @@
-resource "aws_api_gateway_api_key" "example" {
-  name    = "example_api_key"
-  enabled = true
-}
+# resource "aws_api_gateway_api_key" "example" {
+#   name    = "example_api_key"
+#   enabled = true
+# }
 
-resource "aws_api_gateway_usage_plan" "example" {
-  name       = "example_usage_plan"
-  depends_on = [aws_api_gateway_deployment.example]
+# resource "aws_api_gateway_usage_plan" "example" {
+#   name       = "example_usage_plan"
+#   depends_on = [aws_api_gateway_deployment.example]
 
-  api_stages {
-    api_id = aws_api_gateway_rest_api.discord_endpoint.id
-    stage  = aws_api_gateway_deployment.example.stage_name
-  }
-}
+#   api_stages {
+#     api_id = aws_api_gateway_rest_api.discord_endpoint.id
+#     stage  = aws_api_gateway_deployment.example.stage_name
+#   }
+# }
 
-resource "aws_api_gateway_usage_plan_key" "example" {
-  key_id        = aws_api_gateway_api_key.example.id
-  key_type      = "API_KEY"
-  usage_plan_id = aws_api_gateway_usage_plan.example.id
-}
+# resource "aws_api_gateway_usage_plan_key" "example" {
+#   key_id        = aws_api_gateway_api_key.example.id
+#   key_type      = "API_KEY"
+#   usage_plan_id = aws_api_gateway_usage_plan.example.id
+# }
