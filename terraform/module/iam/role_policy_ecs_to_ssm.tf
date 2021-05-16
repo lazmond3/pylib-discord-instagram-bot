c270d86eedaae95f63abe3bc1954dd86d74d89d2
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-16 13:07:10 +0900

    [terraform] [iam-ssm] ssm:DescribeParameters を追加してみる | fix typo 2021 => 2012

--- terraform/module/iam/role_policy_ecs_to_ssm.tf
+++ terraform/module/iam/role_policy_ecs_to_ssm.tf
@@ -3,14 +3,21 @@ resource "aws_iam_role_policy" "ecs_to_ssm" {
   role = var.ecs_task_execution_role_id
 
   policy = jsonencode({
-    Version = "2021-10-17"
+    Version = "2012-10-17"
     Statement = [
       {
         Action = [
           "ssm:GetParameters"
         ]
-        EFfect   = "Allow"
+        Effect   = "Allow"
         Resource = var.secret_arns
+      },
+      {
+        Action = [
+          "ssm:DescribeParameters"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
       }
     ]
   })
