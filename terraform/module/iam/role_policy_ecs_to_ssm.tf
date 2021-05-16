4c4e5c5057cae0c984fde15d0a05cba1fe48950d
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-16 12:52:55 +0900

    [terraform] [iam] update ecs から ssm へのアクセス阻害について、jsonencode をつかってみた　

--- terraform/module/iam/role_policy_ecs_to_ssm.tf
+++ terraform/module/iam/role_policy_ecs_to_ssm.tf
@@ -1,23 +1,34 @@
 resource "aws_iam_role_policy" "ecs_to_ssm" {
-  name  = "ecs_to_ssm"
-  count = length(var.secret_arns)
-
+  name = "ecs_to_ssm"
   role = var.ecs_task_execution_role_id
 
-  policy = <<-EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
+  policy = jsonencode({
+    Version = "2021-10-17"
+    Statement = [
       {
-        "Action": [
+        Action = [
           "ssm:GetParameters"
-        ],
-        "Effect": "Allow",
-        "Resource": [
-          "${var.secret_arns[count.index]}"
         ]
+        EFfect   = "Allow"
+        Resource = var.secret_arns
       }
     ]
-  }
-  EOF
+  })
+
+  # <<-EOF
+  # {
+  #   "Version": "2012-10-17",
+  #   "Statement": [
+  #     {
+  #       "Action": [
+  #         "ssm:GetParameters"
+  #       ],
+  #       "Effect": "Allow",
+  #       "Resource": [
+  #         "${var.secret_arns[count.index]}"
+  #       ]
+  #     }
+  #   ]
+  # }
+  # EOF
 }
