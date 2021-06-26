ed29b4a120c07da05fae44999c43d9a59a5d74fd
lazmond3 <lazmond3@users.noreply.github.com>
2021-06-26 20:15:48 +0900

    [terraform] TODO コメント削除

--- terraform/module/ssm/ssm_discord.tf
+++ terraform/module/ssm/ssm_discord.tf
@@ -1,37 +1,37 @@
 resource "aws_ssm_parameter" "token" {
   name  = "token"
   type  = "String"
-  value = var.token # TODO: FIX これTF_VAR_由来にしたい.
+  value = var.token
 }
 
 resource "aws_ssm_parameter" "consumer_key" {
   name  = "consumer_key"
   type  = "String"
-  value = var.consumer_key # TODO: FIX これTF_VAR_由来にしたい.
+  value = var.consumer_key
 }
 
 resource "aws_ssm_parameter" "consumer_secret" {
   name  = "consumer_secret"
   type  = "String"
-  value = var.consumer_secret # TODO: FIX これTF_VAR_由来にしたい.
+  value = var.consumer_secret
 }
 resource "aws_ssm_parameter" "mid" {
   name  = "mid"
   type  = "String"
-  value = var.mid # TODO: FIX これTF_VAR_由来にしたい.
+  value = var.mid
 }
 resource "aws_ssm_parameter" "sessionid" {
   name  = "sessionid"
   type  = "String"
-  value = var.sessionid # TODO: FIX これTF_VAR_由来にしたい.
+  value = var.sessionid
 }
 resource "aws_ssm_parameter" "v_aws_access_key_id" {
   name  = "v_aws_access_key_id"
   type  = "String"
-  value = var.aws_access_key_id # TODO: FIX これTF_VAR_由来にしたい.
+  value = var.aws_access_key_id
 }
 resource "aws_ssm_parameter" "v_aws_secret_access_key" {
   name  = "v_aws_secret_access_key"
   type  = "String"
-  value = var.aws_secret_access_key # TODO: FIX これTF_VAR_由来にしたい.
+  value = var.aws_secret_access_key
 }
