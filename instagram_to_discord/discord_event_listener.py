da7201e6cf9bfbb192670707c8d9dfa0dd929533
lazmond3 <lazmond3@users.noreply.github.com>
2021-07-11 10:28:02 +0900

    instagram で、 p URL でも 動画のことがあるので修正する

--- instagram_to_discord/discord_event_listener.py
+++ instagram_to_discord/discord_event_listener.py
@@ -127,7 +127,8 @@ class DiscordMessageListener(discord.Client):
 
         if "instagram-support" in message.author.display_name: return
 
-        if "https://www.instagram.com/reel/" in content:
+        if "https://www.instagram.com/" in content and \
+            ("/p/" in content or "/reel/" in content):
             print("[log] channel name: ", message.channel.name)
             extracted_base_url = instagram_extract_from_content(content)
             if not extracted_base_url:
@@ -166,41 +167,28 @@ class DiscordMessageListener(discord.Client):
                     except Exception as e:
                         print("[instagram video upload] file send error!  : ",  e)
                 os.remove(fname_video)
+            else:
+                images = get_multiple_medias_from_str(text)
 
+                print("[DEBUG] images: ", images)
+                insta_obj = instagran_parse_json_to_obj(text)
 
-        elif "https://www.instagram.com/p/" in content:
-            print("[log] channel name: ", message.channel.name)
-            extracted_base_url = instagram_extract_from_content(content)
-            if not extracted_base_url:
-                print("[error] failed to parse base_url for : ", content)
-                return
-            a_url = convert_instagram_url_to_a(extracted_base_url)
-            self.last_url_instagram[channel] = a_url
-            self.is_twitter_last = False
+                msg_list = content.split()
+                nums = []
+                if len(msg_list) > 1:
+                    nums = msg_list[1].split(",")
+                    nums = map(lambda x: int(x), nums)
+                    nums = list(nums)
+                else:
+                    nums.append(1)
+                assert(nums[0] >= 1)
 
-            text = requests_get_cookie(url=a_url)
-            msg_list = content.split()
-            nums = []
-            if len(msg_list) > 1:
-                nums = msg_list[1].split(",")
-                nums = map(lambda x: int(x), nums)
-                nums = list(nums)
-            else: 
-                nums.append(1)
-
-            print("[DEBUG} nums!: ", nums)
-
-            images = get_multiple_medias_from_str(text)
-
-            print("[DEBUG] images: ", images)
-            insta_obj = instagran_parse_json_to_obj(text)
-            assert(nums[0] >= 1)
-            image_url = images[nums[0]-1]
-            insta_obj.media = image_url
-            embed = self.create_instagram_pic_embed(insta_obj, extracted_base_url)
-            await message.channel.send(embed=embed)
-            if len(nums) == 1: return
-            await self.send_instagram_images_for_specified_index(images, nums[1:], message)
+                image_url = images[nums[0]-1]
+                insta_obj.media = image_url
+                embed = self.create_instagram_pic_embed(insta_obj, extracted_base_url)
+                await message.channel.send(embed=embed)
+                if len(nums) == 1: return
+                await self.send_instagram_images_for_specified_index(images, nums[1:], message)
 
         elif ("https://twitter.com/" in content and "/status/" in content):
             # ここで最後のtwitter url を記録しておく。
