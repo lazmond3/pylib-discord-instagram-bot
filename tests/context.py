e16feec1ab0f5c787816d79ffa37a6d12c061019
lazmond3 <lazmond3@users.noreply.github.com>
2021-07-20 18:53:19 +0900

    tests のフォーマット

--- tests/context.py
+++ tests/context.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import instagram_to_discord
-from instagram_to_discord import instagram_type
-import instagram_to_discord.twitter
-
-import sys
 import os
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..')))
+import sys
+
+import instagram_to_discord
+import instagram_to_discord.twitter
+from instagram_to_discord import instagram_type
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
