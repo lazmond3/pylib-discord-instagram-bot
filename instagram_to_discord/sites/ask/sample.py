a6c43ba0cee2471bcf6de5bad1f2eb3e474bc439
lazmond3 <lazmond3@users.noreply.github.com>
2021-12-02 15:57:53 +0900

    [sites.ask] sample add koba の答えを抽出

--- instagram_to_discord/sites/ask/sample.py
+++ instagram_to_discord/sites/ask/sample.py
@@ -1,9 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
-    target_url = "https://lap78.ask.fm/igoto/45DKECPW7B667HQMHN2IG6NM6SDD7LCJ2P667BP7D7SLNZN62UYY2Z2IF5H4YRZSKC67YFQIJQUH4DB7YEV7M2VONS5LPBQLUIORJE2YHVWSCKGPBOHXYPXKSC27R6LPZSRYGEIXRKEH3H4IBOBB6QK46PGHLYM4ZDPVJMTRQSO2OPPSIUYFB5HK5NNIJ4H3TI43KFDU4EKQ===="
-    data = requests.get(target_url)
-    text = data.text
-    print(f"type of text: {type(text)}")
-    with open("dump_ask1.txt", "w") as f:
-        f.write(text)
+    # target_url = "https://lap78.ask.fm/igoto/45DKECPW7B667HQMHN2IG6NM6SDD7LCJ2P667BP7D7SLNZN62UYY2Z2IF5H4YRZSKC67YFQIJQUH4DB7YEV7M2VONS5LPBQLUIORJE2YHVWSCKGPBOHXYPXKSC27R6LPZSRYGEIXRKEH3H4IBOBB6QK46PGHLYM4ZDPVJMTRQSO2OPPSIUYFB5HK5NNIJ4H3TI43KFDU4EKQ===="
+    # data = requests.get(target_url)
+    # text = data.text
+    # print(f"type of text: {type(text)}")
+    # with open("dump_ask1.txt", "w") as f:
+    #     f.write(text)
+    text = ""
+    with open("dump_ask/dump_ask1.txt") as f:
+        text = f.read()
+    # print(f"text: {text}")
+    
+    soup = BeautifulSoup(text, "html.parser")
+
+    r = soup.select_one("div.streamItem_content")
+    print(f"r: ", r.text)
