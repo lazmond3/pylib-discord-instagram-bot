333d8605d255f5cca913463fcea7efa020c89602
lazmond3 <lazmond3@users.noreply.github.com>
2021-12-03 18:17:00 +0900

    [sites.twitter] WIP リファクタ: delete print デバッグ

--- instagram_to_discord/sites/twitter/twitter.py
+++ instagram_to_discord/sites/twitter/twitter.py
@@ -42,8 +42,6 @@ async def send_twitter_images_from_cache_for_specified_index(
             continue
         if skip_one and n == 1:
             continue
-        if DEBUG:
-            print(f"send_twitter_image: url: {image_urls[idx]}")
         embed = create_embed_twitter_image(image_urls[idx])
         await message.channel.send(embed=embed)
 
