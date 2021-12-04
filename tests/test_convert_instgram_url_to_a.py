bc0e8ea3193ea263d6e3a89577c751da8fc881e5
lazmond3 <lazmond3@users.noreply.github.com>
2021-12-04 20:49:39 +0900

    [tests] test_convert_instgram_url_to_a.py

--- tests/test_convert_instgram_url_to_a.py
+++ tests/test_convert_instgram_url_to_a.py
@@ -3,51 +3,50 @@
 # from .context import use_hello
 import unittest
 
-from . import context
+# from . import context
 
-instagram_to_discord = context.instagram_to_discord
+# instagram_to_discord = context.instagram_to_discord
+from instagram_to_discord.sites.instagram import converter_instagram_url
 
-class BasicTestSuite(unittest.TestCase):
-    """Basic test cases."""
 
-    def test_instagram_make_base_url(self):
-        input_str = (
-            "https://www.instagram.com/p/CGEF-ewBxNm/?utm_source=ig_web_copy_link"
+def test_instagram_make_base_url():
+    input_str = (
+        "https://www.instagram.com/p/CGEF-ewBxNm/?utm_source=ig_web_copy_link"
+    )
+    answer = "https://www.instagram.com/p/CGEF-ewBxNm"
+    result = converter_instagram_url.instagram_make_base_url(
+        input_str
+    )
+    assert answer == result
+
+
+def test_instagram_make_author_page():
+    input_usr = "winter_28270"
+    answer = "https://www.instagram.com/winter_28270/"
+    result = (
+        converter_instagram_url.instagram_make_author_page(
+            input_usr
         )
-        answer = "https://www.instagram.com/p/CGEF-ewBxNm"
-        result = instagram_to_discord.converter_instagram_url.instagram_make_base_url(
-            input_str
+    )
+    assert answer == result
+
+
+def test_extract_from_content():
+    input_content = """
+        こんにちは。データは https://www.instagram.com/p/CGEF-ewBxNm/?utm_source=ig_web_copy_link です。
+    """
+    answer = "https://www.instagram.com/p/CGEF-ewBxNm"
+    result = (
+        converter_instagram_url.instagram_extract_from_content(
+            input_content
         )
-        assert answer == result
-
-    def test_instagram_make_author_page(self):
-        input_usr = "winter_28270"
-        answer = "https://www.instagram.com/winter_28270/"
-        result = (
-            instagram_to_discord.converter_instagram_url.instagram_make_author_page(
-                input_usr
-            )
-        )
-        assert answer == result
-
-    def test_extract_from_content(self):
-        input_content = """
-            こんにちは。データは https://www.instagram.com/p/CGEF-ewBxNm/?utm_source=ig_web_copy_link です。
-        """
-        answer = "https://www.instagram.com/p/CGEF-ewBxNm"
-        result = (
-            instagram_to_discord.converter_instagram_url.instagram_extract_from_content(
-                input_content
-            )
-        )
-        assert result == answer
-
-    def test_extract_url(self):
-        input_content = """こんにちは。データは https://www.instagram.com/p/CGEF-ewBxNm/?utm_source=ig_web_copy_link です。"""
-        answer = "https://www.instagram.com/p/CGEF-ewBxNm"
-        result = instagram_to_discord.converter_instagram_url.extract_url(input_content)
-        assert answer == result
+    )
+    assert result == answer
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_extract_url():
+    input_content = """こんにちは。データは https://www.instagram.com/p/CGEF-ewBxNm/?utm_source=ig_web_copy_link です。"""
+    answer = "https://www.instagram.com/p/CGEF-ewBxNm"
+    result = converter_instagram_url.extract_url(
+        input_content)
+    assert answer == result
