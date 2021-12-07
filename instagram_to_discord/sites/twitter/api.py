2306fd93449d4bcda8f27074331a801cabe9b03e
lazmond3 <lazmond3@users.noreply.github.com>
2021-12-07 14:49:15 +0900

    [sites.twitter.api] user のタイムラインを取得。非公開ユーザは取れなかった。

--- instagram_to_discord/sites/twitter/api.py
+++ instagram_to_discord/sites/twitter/api.py
@@ -4,7 +4,7 @@ from .twitter_image import TwitterImage, convert_twitter
 from .base64_util import base64_encode_str
 from ...const_value import IS_DEBUG
 import requests
-from typing import Any, Dict, Optional, cast
+from typing import Any, Dict, Optional, cast, List
 import os
 import json
 from logging import LogRecord, getLogger, StreamHandler, DEBUG, INFO
@@ -14,6 +14,13 @@ handler.setLevel(DEBUG)
 logger.setLevel(DEBUG)
 logger.addHandler(handler)
 logger.propagate = False
+
+
+def mkdir_notexists(dirs: List[str]):
+    for dirpath in dirs:
+        if not os.path.exists(dirpath):
+            os.makedirs(dirpath)
+            logger.info(f"[mkdir_noexists] mkdir {dirpath}")
 
 
 if not IS_DEBUG:
@@ -110,6 +117,65 @@ def get_one_tweet(tweet_id: str, is_second: bool = False) -> TwitterImage:
     tw = convert_twitter(js)
     logger.debug(f"video: {tw.video_url} images: {','.join(tw.image_urls)}, ")
     return tw
+
+
+def get_tweets_of_user(
+        screen_name: str,
+        since_id: Optional[str] = None,  # 最小の、この要素を含めないid。これより新しいツイートが検索される
+        max_id: Optional[str] = None,  # これを含めるかこれ以下の
+        is_second: bool = False
+):
+    logger.debug(f"[get_tweets_of_user] screen_name: {screen_name}")
+    if not os.path.exists(TOKEN_FILENAME):
+        get_auth_wrapper()
+    with open(TOKEN_FILENAME) as f:
+        s = json.load(f)
+        token = s["access_token"]
+
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+    params = {"screen_name": screen_name, "tweet_mode": "extended"}
+
+    if since_id:
+        params["since_id"] = since_id  # nopep8
+    if max_id:
+        params["max_id"] = max_id  # nopep8
+
+    headers = {"Authorization": f"Bearer {token}"}
+
+    # キャッシュを利用する.
+    mkdir_notexists([f"dump_tweet_of_user"])
+    fname = f"dump_tweet_of_user/dump_user_{screen_name}.json"
+    if os.path.exists(fname):
+        with open(fname, 'r') as f:
+            js = json.load(f)
+        # tw = convert_twitter(js)
+        return
+
+    # r = requests.get(url, params=params, headers=headers)
+    r = None
+    try:
+        r = requests.get(url, params=params, headers=headers)
+    except Exception:
+        print(f"error exception?? : {r.status_code}")
+        if not is_second:
+            os.remove(TOKEN_FILENAME)
+            get_auth_wrapper()
+            # もう一度実行する
+            get_tweets_of_user(screen_name, since_id, max_id, True)
+
+    tx = r.text
+    print(f"tx: {tx}")
+    js = text_to_dict(tx)
+
+    with open(fname, "w", encoding="utf-8") as f:
+        json.dump(js, f, ensure_ascii=False)
+    # 直し方がよくわからないので、json の結果を利用させてもらう。
+    # with open(fname) as f:
+        # txt_decoded = f.read()
+
+    # tw = convert_twitter(js)
+    # logger.debug(f"video: {tw.video_url} images: {','.join(tw.image_urls)}, ")
+    # return tw
 
 
 def get_sumatome(tweet_id: str, is_second: bool = False) -> None:
