a4e191692fe549fb1d269523485102d659cbd51a
lazmond3 <lazmond3@users.noreply.github.com>
2022-02-19 15:36:51 +0900

    [embed.py] add new function create_twitter_description_image

--- instagram_to_discord/util2/embed.py
+++ instagram_to_discord/util2/embed.py
@@ -1,6 +1,8 @@
+from typing import Optional
 import discord
 from instagram_to_discord.sites.instagram.converter_instagram_url import instagram_make_author_page
 from instagram_to_discord.sites.instagram.instagram_type import InstagramData
+from instagram_to_discord.sites.twitter.twitter_image import TwitterImage
 from instagram_to_discord.string_util import sophisticate_string
 
 
@@ -66,4 +68,20 @@ def create_instagram_video_embed(obj: InstagramData, base_url: str):
 def create_twitter_image_embed(image_url: str):
     embed = discord.Embed(color=discord.Color.blue())
     embed.set_image(url=image_url)
+    return embed
+
+def create_twitter_description_image(tw: TwitterImage, image_url: Optional[str] = None ) -> discord.Embed:
+    embed = discord.Embed(
+        title=tw.user_display_name,
+        description=tw.text,
+        url=f"https://twitter.com/{tw.user_screen_name}",
+        color=discord.Color.blue(),
+    )
+    if image_url:
+        embed.set_image(url=image_url)
+    embed.set_author(
+        name=tw.user_display_name,
+        url=f"https://twitter.com/{tw.user_screen_name}",
+        icon_url=tw.user_profile_image_url,
+    )
     return embed
