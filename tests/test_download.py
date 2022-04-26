a073a088b0b4119ca630c1d42dc2fa1680767b8a
lazmond3 <moikilo00@gmail.com>
2022-04-26 11:28:32 +0900

    [test_download.py]

--- tests/test_download.py
+++ tests/test_download.py
@@ -4,8 +4,6 @@
 import json
 import unittest
 
-from debug import DEBUG
-
 from instagram_to_discord.sites.instagram.instagram_type import convert_long_caption
 from instagram_to_discord.sites.twitter.twitter_image import convert_twitter
 from instagram_to_discord.download import make_instagram_mp4_filename
