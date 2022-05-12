3853ba5d3c28652e91cca93d418ffdfbf0ea099b
lazmond3 <moikilo00@gmail.com>
2022-05-13 06:25:32 +0900

    [logging.py]

--- instagram_to_discord/logging.py
+++ instagram_to_discord/logging.py
@@ -1,23 +1,24 @@
 import logging
+import os
 from cmreslogging.handlers import CMRESHandler
 
-# handler = CMRESHandler(hosts=[{'host': 'localhost', 'port': 9200}],
-#                        auth_type=CMRESHandler.AuthType.BASIC_AUTH,
-#                        es_index_name="my_python_index")
+ES_HOST = os.getenv("ES_HOST")
+ES_USER_NAME = os.getenv("ES_USER_NAME")
+ES_PASSWORD  = os.getenv("ES_PASSWORD")
 
 handler = CMRESHandler(
     hosts=[
         {
-            "host": "search-discord-log2-e22jfdiyin6rblshxfml3hcqaa.ap-northeast-1.es.amazonaws.com",
+            "host": ES_HOST,
             "port": 443,
         }
     ],
     auth_type=CMRESHandler.AuthType.BASIC_AUTH,
-    es_index_name="my_python_index1",
+    es_index_name="discord_instagram",
     use_ssl=True,
-    auth_details=("user1", "]iQFWF^q]=4uw8-"),
-    es_additional_fields={"App": "MyAppName", "Environment": "Dev"},
+    auth_details=(ES_USER_NAME, ES_PASSWORD),
+    es_additional_fields={"App": "DiscordInsta", "Environment": "Dev"},
 )
-log = logging.getLogger("PythonTest1")
+log = logging.getLogger("GlobalLogger")
 log.setLevel(logging.DEBUG)
 log.addHandler(handler)
