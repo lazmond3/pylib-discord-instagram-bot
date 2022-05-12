64c35640cd09a09ad1e26af907c842ab2835ac25
lazmond3 <moikilo00@gmail.com>
2022-05-13 07:18:04 +0900

    [logging.py] local logging

--- instagram_to_discord/logging.py
+++ instagram_to_discord/logging.py
@@ -8,28 +8,41 @@ ES_PASSWORD  = os.getenv("ES_PASSWORD")
 ENV = os.getenv("ENV")
 
 
-if ENV != "local":
-    handler = CMRESHandler(
-        hosts=[
-            {
-                "host": ES_HOST,
-                "port": 443,
-            }
-        ],
-        auth_type=CMRESHandler.AuthType.BASIC_AUTH,
-        es_index_name="discord_instagram",
-        use_ssl=True,
-        auth_details=(ES_USER_NAME, ES_PASSWORD),
-        es_additional_fields={"App": "DiscordInsta", "Environment": ENV},
-    )
-    log = logging.getLogger("GlobalLogger")
-    # 本当は、PRODだったらsetLevel: Info とかでもよい。
-    log.setLevel(logging.DEBUG)
-    log.addHandler(handler)
+handler = CMRESHandler(
+    hosts=[
+        {
+            "host": ES_HOST,
+            "port": 443,
+        }
+    ],
+    auth_type=CMRESHandler.AuthType.BASIC_AUTH,
+    es_index_name="discord_instagram",
+    use_ssl=True,
+    auth_details=(ES_USER_NAME, ES_PASSWORD),
+    es_additional_fields={"App": "DiscordInsta", "Environment": ENV},
+)
+logger = logging.getLogger("GlobalLogger")
+# 本当は、PRODだったらsetLevel: Info とかでもよい。
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+
+class Log:
+    def __init__(self, logger):
+        self.logger = logger
+    def debug(self, x):
+        self.logger.debug(x)
+        print(f"[debug] {x}")
+    def info(self, x):
+        self.logger.info(x)
+        print(f"[info] {x}")
+    def warning(self, x):
+        self.logger.warning(x)
+        print(f"[warning] {x}")
+    def error(self, x):
+        self.logger.error(x)
+        print(f"[error] {x}")
+
+if ENV == "local":
+    log = Log(logger)
 else:
-    class Log:
-        def debug(self, x): print(f"[debug] {x}")
-        def info(self, x): print(f"[info] {x}")
-        def warning(self, x): print(f"[warning] {x}")
-        def error(self, x): print(f"[error] {x}")
-    log = Log()
+    log = logger
